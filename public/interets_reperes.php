94d38d790ab7c53092a91bb73fd7192bb48ced98
Mohannad-Adham <mohannad.adm@gmail.com>
2017-08-02 11:52:00 +0200

    set the design of the third game for mobile devices

--- public/interets_reperes.php
+++ public/interets_reperes.php
@@ -69,16 +69,19 @@
                         </div>
                     </div>
                 </div> -->
-                    <div class="col-xs-12 col-sm-6 col-sm-push-3 col-md-4 col-md-push-4">
-                        <?php include_once("timer.php"); ?>
-                    </div>
-                    <div class="col-xs-6 col-sm-3 col-sm-pull-6 col-md-4 col-md-pull-4">
+
+                    <div class="col-xs-6 col-sm-3  col-md-4 ">
                         <button id="btn-interets" class="btn btn-lg btn-danger"> Points d'Interets </button>
                     </div>
-
-                    <div class="col-xs-6 col-sm-3 col-md-4">
+                    <div class="col-xs-6 col-sm-3 col-sm-push-6 col-md-4 col-md-push-4">
                         <button id="btn-reperes" class="btn btn-lg btn-primary">Points de reperes</button>
                     </div>
+
+                    <div class="col-xs-12 col-sm-6 col-sm-pull-3 col-md-4 col-md-pull-4">
+                        <?php include_once("timer.php"); ?>
+                    </div>
+
+
             </div>
         </div>
       </div>
